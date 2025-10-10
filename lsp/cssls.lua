5e5697b5e34ece3f9b259276c924e7c8c0575234
christianupdoc <christian@updoc.com.au>
2025-10-10 17:13:37 +1100

    clj

--- lsp/cssls.lua
+++ lsp/cssls.lua
@@ -4,7 +4,7 @@ return {
   init_options = {
     provideFormatter = true
   },
-  root_markets = { "package.json", ".git" },
+  root_markers = { "package.json", ".git" },
   settings = {
     css = {
       validate = true
