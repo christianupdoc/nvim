7ff86449d3ef8f0a4d065307c6c94565a424aeb9
christianupdoc <christian@updoc.com.au>
2026-04-07 12:38:17 +1000

    .

--- plugin/treesitter.lua
+++ plugin/treesitter.lua
@@ -1,6 +1,5 @@
 vim.treesitter.language.register('swift', 'swift')
 
-local installed_cache = {}
 local skip_ft = {
   [""] = true, notify = true, noice = true, lazy = true, mason = true,
   NvimTree = true, TelescopePrompt = true, quickfix = true, help = true,
@@ -11,19 +10,6 @@ vim.api.nvim_create_autocmd("FileType", {
   callback = function(args)
     local ft = vim.bo[args.buf].filetype
     if skip_ft[ft] then return end
-
-    local lang = vim.treesitter.language.get_lang(ft) or ft
-
-    if not installed_cache[lang] and not pcall(vim.treesitter.language.inspect, lang) then
-      installed_cache[lang] = true
-      local available = require("nvim-treesitter").get_available()
-      if not vim.list_contains(available, lang) then return end
-      vim.schedule(function()
-        require("nvim-treesitter").install({ lang })
-      end)
-      return
-    end
-
     pcall(vim.treesitter.start, args.buf)
   end,
 })
