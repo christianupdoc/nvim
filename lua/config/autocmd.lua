5be4ed84b8b8c38f81ad74f4b8cf20e668c47934
christianupdoc <christian@updoc.com.au>
2026-05-06 13:48:54 +1000

    python indentation

--- lua/config/autocmd.lua
+++ lua/config/autocmd.lua
@@ -98,6 +98,17 @@ vim.api.nvim_create_autocmd("LspAttach", {
 
 })
 
+-- Python
+autocmd("FileType", {
+  pattern = "python",
+  callback = function()
+    vim.opt_local.shiftwidth = 4
+    vim.opt_local.softtabstop = 4
+    vim.opt_local.tabstop = 4
+    vim.opt_local.expandtab = true
+  end,
+})
+
 -- SQL
 autocmd("FileType", {
   pattern = "sql",
